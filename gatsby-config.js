0bfee6015d4eda57f6559de1875feef0778106d9
Soravis Prakkamakul <asunnotthesun@gmail.com>
2018-09-21 15:58:22 -0700

    Fix site meta information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'IMSA',
+    author: 'Information Management Student Association (IMSA)',
+    description: 'The hub for all things I-School',
+    siteUrl: 'https://imsaberkeley.org',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
